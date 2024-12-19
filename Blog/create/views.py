3ecc61a000f483e553ce650bf390fd0ad140b399
Vitya <ccowes050807@gmail.com>
2024-12-20 00:28:01 +0200

    create

--- Blog/create/views.py
+++ Blog/create/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 def render_free(request):
-    return render(request, 'create/free.html')
+    return render(request, 'free.html')
 # Create your views here.
+def render_standard(request):
+    return render(request, 'standard.html')
+
+def render_pro(request):
+    return render(request, 'pro.html')
